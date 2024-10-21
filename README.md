4af871abb02e23af19d6758fff88876621ed9eb6
Moises-Abel <a01799628@tec.mx>
2024-10-21 11:47:02 -0600

    Primeros cambios

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Herramientas-computacionales-el-arte-de-la-anal-tica-
-
-1. elemento
-2. elemento
-3. elemento
-4. elemento
+## markdown
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from herramientas-computacionales-el-arte-de-la-anal-tica-main!")
+
+
+if __name__ == "__main__":
+    main()
